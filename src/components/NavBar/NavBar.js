adcf073f7fc8dfbda4ced1db752ad6a8952e18e9
Minoru98 <minorutamashiro21@gmail.com>
2021-09-15 23:52:30 -0300

    trabajo

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,37 @@
+import logo from '../../assets/logo192.png';
+import './NavBar.css'
+//External components
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const NavBar = () => {
+
+  return (
+    <header>
+        <AppBar position="static" className="main-navbar">
+        <Toolbar>
+          <Typography variant="h6">
+            <div className= "container-logo">
+               <img src= {logo} alt={'logo principal'}/>
+            </div>
+          </Typography>
+          <div>
+              <ul className="navbar-list"> 
+                   <li><Button color="inherit" >Inicio</Button></li>
+                   <li><Button color="inherit" >Producto</Button></li>
+                   <li><Button color="inherit" >Preguntas Frecuentes</Button></li>
+                   <li><Button color="inherit" >Contacto</Button></li>
+              </ul>
+         </div>
+         <div className="container-search">
+             Buscar <input type="search"></input>
+         </div>
+        </Toolbar>
+      </AppBar>   
+    </header>
+  );
+}
+
+export default NavBar;
